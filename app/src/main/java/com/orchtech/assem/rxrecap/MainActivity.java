f235751b0370ba4eea1d31b4f2e009441d9a296f
Mohamed Assem <mo7mad.assim@gmail.com>
2020-06-08 14:34:14 +0200

    Add CompositeDisposable

--- app/src/main/java/com/orchtech/assem/rxrecap/MainActivity.java
+++ app/src/main/java/com/orchtech/assem/rxrecap/MainActivity.java
@@ -8,8 +8,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,10 +24,13 @@ public class MainActivity extends AppCompatActivity {
      * https://www.androidhive.info/RxJava/android-getting-started-with-reactive-programming/
      * source code:
      */
+
     private String TAG = "RxRecapTag";
     // 5.
     // Disposable is used and calling disposable.
     private Disposable disposable;
+    // 7.
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,32 +40,75 @@ public class MainActivity extends AppCompatActivity {
         // 1.
         // Create Observable that emits list of animal names.
         Observable<String> animalsObservable = getAnimalsObservable();
-
-
         // 2.
         // Create an Observer that listen to Observable.
         Observer<String> animalsObserver = getAnimalsObserver();
 
+        // 7.
+        //  animalsObserverDisposable and animalsObserverAllCapsDisposable subscribed to same Observable.
+        //  The both observers receives the same data but the data changes as different operators are applied on the stream.
+        DisposableObserver<String> animalsObserverDisposable = getAnimalsObserverDisposable();
+        DisposableObserver<String> animalsObserverAllCapsDisposable = getAnimalsAllCapsObserverDisposable();
+
+
         // 4.
         // Make Observer subscribe to Observable so that it can start receiving the data.
         // Here, you can notice two more methods, observeOn() and subscribeOn().
-        animalsObservable
-                // This tell the Observable to run the task on a background thread.
-                .subscribeOn(Schedulers.io())
-                // This tells the Observer to receive the data on android UI thread
-                // so that you can take any UI related actions.
-                .observeOn(AndroidSchedulers.mainThread())
-                // 6.
-                // Adding filters
-                //  filter() operator is used to filter out the emitted data
-                .filter(new Predicate<String>() {
-                    @Override
-                    public boolean test(String s) throws Exception {
-                        return s.toLowerCase().startsWith("b");
-                    }
-                })
-                .subscribe(animalsObserver);
+//        animalsObservable
+//                // This tell the Observable to run the task on a background thread.
+//                .subscribeOn(Schedulers.io())
+//                // This tells the Observer to receive the data on android UI thread
+//                // so that you can take any UI related actions.
+//                .observeOn(AndroidSchedulers.mainThread())
+//                // 6.
+//                // Adding filters
+//                //  filter() operator is used to filter out the emitted data
+//                .filter(new Predicate<String>() {
+//                    @Override
+//                    public boolean test(String s) {
+//                        return s.toLowerCase().startsWith("b");
+//                    }
+//                })
+//                .subscribe(animalsObserver);
 
+
+        /**
+         * filter() is used to filter out the animal names starting with `b`
+         * */
+        compositeDisposable.add(
+                animalsObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(String s) throws Exception {
+                                return s.toLowerCase().startsWith("b");
+                            }
+                        })
+                        .subscribeWith(animalsObserverDisposable)
+        );
+
+        /**
+         * filter() is used to filter out the animal names starting with 'c'
+         * map() is used to transform all the characters to UPPER case
+         * */
+        compositeDisposable.add(
+                animalsObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(String s) throws Exception {
+                                return s.toLowerCase().startsWith("c");
+                            }
+                        })
+                        .map(new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                return s.toUpperCase();
+                            }
+                        })
+                        .subscribeWith(animalsObserverAllCapsDisposable));
     }
 
     // 1.
@@ -109,12 +158,57 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
+    // 7.
+    private DisposableObserver<String> getAnimalsObserverDisposable() {
+        return new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
+    }
+
+    // 7.
+    private DisposableObserver<String> getAnimalsAllCapsObserverDisposable() {
+        return new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
+    }
+
     // 5.
     // dispose() in onDestroy() will un-subscribe the Observer.
+    // 7.
+    // CompositeDisposable: Can maintain list of subscriptions in a pool and can dispose them all at once
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // don't send events once the activity is destroyed
         disposable.dispose();
+
+        // 7.
+        compositeDisposable.clear();
     }
 }
